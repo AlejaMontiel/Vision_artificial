a35cc8c737f501d65aa31a2e454e49ec59ab66af
AlejaMontiel <167145236+AlejaMontiel@users.noreply.github.com>
2024-09-30 22:22:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,50 +3,39 @@ import yolov5
 import streamlit as st
 import numpy as np
 import pandas as pd
-from ultralytics import YOLO
 
-import sys
-sys.path.append('./ultralytics/yolo')
-
-from utils.checks import check_requirements
-
-
-# load pretrained model
+# Cargar el modelo previamente entrenado
 model = yolov5.load('yolov5s.pt')
-model = yolov5.load('yolov5nu.pt')
 
-# set model parameters
-model.conf = 0.25  # NMS confidence threshold
-model.iou = 0.45  # NMS IoU threshold
-model.agnostic = False  # NMS class-agnostic
-model.multi_label = False  # NMS multiple labels per box
-model.max_det = 1000  # maximum number of detections per image
+# Establecer parámetros del modelo
+model.conf = 0.25  # Umbral de confianza NMS
+model.iou = 0.45  # Umbral IoU NMS
+model.agnostic = False  # NMS clase-agnóstica
+model.multi_label = False  # NMS múltiples etiquetas por caja
+model.max_det = 1000  # Número máximo de detecciones por imagen
 
-# take a picture with the camera
+# Interfaz de la aplicación
 st.title("Detección de Objetos en Imágenes")
 
+# Parámetros de configuración
 with st.sidebar:
-            st.subheader('Parámetros de Configuración')
-            model.iou= st.slider('Seleccione el IoU',0.0, 1.0)
-            st.write('IOU:', model.iou)
+    st.subheader('Parámetros de Configuración')
+    model.iou = st.slider('Seleccione el IoU', 0.0, 1.0)
+    st.write('IOU:', model.iou)
+    model.conf = st.slider('Seleccione el Confidence', 0.0, 1.0)
+    st.write('Conf:', model.conf)
 
-with st.sidebar:
-            model.conf = st.slider('Seleccione el Confidence',0.0, 1.0)
-            st.write('Conf:', model.conf)
-
-
-picture = st.camera_input("Capturar foto",label_visibility='visible' )
+# Capturar imagen con la cámara
+picture = st.camera_input("Capturar foto", label_visibility='visible')
 
 if picture:
-    #st.image(picture)
-
     bytes_data = picture.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-  
-    # perform inference
+
+    # Realizar inferencia
     results = model(cv2_img)
 
-    # parse results
+    # Parsear resultados
     predictions = results.pred[0]
     boxes = predictions[:, :4] 
     scores = predictions[:, 4]
@@ -55,30 +44,30 @@ if picture:
     col1, col2 = st.columns(2)
 
     with col1:
-        # show detection bounding boxes on image
+        # Mostrar imagen con detecciones
         results.render()
-        # show image with detections 
-        st.image(cv2_img, channels = 'BGR')
+        st.image(cv2_img, channels='BGR')
 
-    with col2:      
-
-        # get label names
+    with col2:
+        # Obtener nombres de las etiquetas
         label_names = model.names
-        # count categories
         category_count = {}
+
+        # Contar categorías
         for category in categories:
             if category in category_count:
                 category_count[category] += 1
             else:
                 category_count[category] = 1        
 
-        data = []        
-        # print category counts and labels
+        data = []
+        # Imprimir recuentos de categorías y etiquetas
         for category, count in category_count.items():
             label = label_names[int(category)]            
-            data.append({"Categoría":label,"Cantidad":count})
-        data2 =pd.DataFrame(data)
+            data.append({"Categoría": label, "Cantidad": count})
         
-        # agrupar los datos por la columna "categoria" y sumar las cantidades
-        df_sum = data2.groupby('Categoría')['Cantidad'].sum().reset_index() 
-        df_sum
+        data2 = pd.DataFrame(data)
+        
+        # Agrupar los datos por la columna "Categoría" y sumar las cantidades
+        df_sum = data2.groupby('Categoría')['Cantidad'].sum().reset_index()
+        st.write(df_sum)
